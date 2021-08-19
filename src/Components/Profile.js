b4d742c82b78bcbb52e022fb94ef28d1a4700527
elodiebhs <elodiebouthors@hotmail.com>
2021-08-18 17:34:16 -0700

    display profil picture

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -33,7 +33,10 @@ export class Profile extends React.Component {
 
     return (
       <div className={className}>
-        <div className="profile-picture"></div>
+        <div className="profile-picture">
+
+          {isLoading === false && <img src={this.state.userData.profilePictureUrl} alt="" />}
+        </div>
         <div className="profile-body">
           <h2>{name}</h2>
           <h3>@{this.props.username}</h3>
