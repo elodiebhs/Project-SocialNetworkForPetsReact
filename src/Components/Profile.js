b0ace13423cc33593415cd09134c1fd416624277
elodiebhs <elodiebouthors@hotmail.com>
2021-08-18 17:24:24 -0700

    Displaying Bio data

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -21,7 +21,9 @@ export class Profile extends React.Component {
     const isLoading = this.state.userData === null ? true : false;
 
     const name = isLoading ? 'Loading...' : this.state.userData.name;
-    
+
+    const bio = isLoading ? 'Loading...' : this.state.userData.bio;
+
     let className = 'Profile';
     if (true) {
       className += ' loading';
@@ -33,7 +35,7 @@ export class Profile extends React.Component {
         <div className="profile-body">
           <h2>{name}</h2>
           <h3>@{this.props.username}</h3>
-          <p>Bio goes here</p>
+          <p>{bio}</p>
           <h3>My friends</h3>
           <Userlist usernames={[]} onChoose={this.props.onChoose} />
         </div>
